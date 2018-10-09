33e36228ddb733a216b5498fd180d97397e323c7
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2018-10-09 11:23:19 -0700

    Fixed jacob

--- src/events/index.php
+++ src/events/index.php
@@ -5,6 +5,7 @@
 	<title>Events</title>
 			<meta charset="utf-8">
     		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" type="text/stylesheet" href="../stylesheets/index.css">
     		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     		<link rel="stylesheet" type="text/css" href="/stylesheets/index.css">
 
@@ -18,7 +19,11 @@
 	<?php include'../navbar.php';?>
 
 	<div class="text-center container">
-		<iframe src="https://calendar.google.com/calendar/embed?src=rocklinusd.org_7urp5rm04s95lpo4qjci83bb2s%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="" height="600" frameborder="0" scrolling="no"></iframe>
+		<iframe src="https://calendar.google.com/calendar/embed?src=rocklinusd.org_7urp5rm04s95lpo4qjci83bb2s%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="100%" height="" frameborder="0" scrolling="no"></iframe>
 	</div>
+
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
 </html>
